be9abb535ae998862d3563b4eb2fa03310644b22
Facu Molas <facundomolasymolas@gmail.com>
2022-07-16 16:04:43 +0200

    agregamos componentes

---
+++ src/componentes/CartWidget.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CartWidget = () => {
+  return (
+    <>
+        <button type="button" class="btn">
+            <i class="bi bi-cart"></i>
+            <span class="badge text-bg-secondary">4</span>
+        </button>
+    </>
+  )
+}
+
+export default CartWidget;
