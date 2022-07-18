5628edef1bd2c1ed14d1911be8cfcbbb0f99a873
Facu Molas <facundomolasymolas@gmail.com>
2022-07-18 17:30:06 +0200

    optimizando

--- src/contenedores/Productos.jsx
+++ src/contenedores/Productos.jsx
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react';
 const Productos = () => {
 
     const [data, setData]=useState();
+    
 
     useEffect(()=>{
 
         fetch('https://api.mercadolibre.com/sites/MLA/search?q=mate')
         .then(response => response.json())
         .then(response => setData(response.results))
-        .catch(err => console.log(err))
+        .catch(err => console.log("error"))
 
         console.log(data);
     },[]);
@@ -19,6 +20,17 @@ const Productos = () => {
   return (
     <>
             <h2>Productos:</h2>
+            <ul>
+            {   
+                data.length > 0 ?
+                data.map(item => (
+                    <li>{item.title}</li>
+                )) :
+                <p>Cargando...</p>
+
+
+            }
+            </ul>
     </>
   )
 }
